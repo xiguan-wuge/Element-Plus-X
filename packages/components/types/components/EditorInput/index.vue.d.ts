5c70bfc4f2168e717e67c3f0df66e31936e927fb
xiguan wuge <1584077483@qq.com>
2026-03-22 02:02:10 +0800

    更新文件

---
+++ packages/components/types/components/EditorInput/index.vue.d.ts
@@ -0,0 +1,45 @@
+import { EditorInputProps } from './types.d.ts';
+declare function focus(e: Event): void;
+declare function blur(e: Event): void;
+declare function select(): void;
+declare function clear(): void;
+declare const _default: import('vue').DefineComponent<EditorInputProps, {
+    focus: typeof focus;
+    blur: typeof blur;
+    select: typeof select;
+    clear: typeof clear;
+    selectAll: () => void;
+    moveToStart: () => void;
+    moveToEnd: () => void;
+    editableElement: globalThis.Ref<HTMLElement | null, HTMLElement | null>;
+}, {}, {}, {}, import('vue').ComponentOptionsMixin, import('vue').ComponentOptionsMixin, {
+    "update:modelValue": (...args: any[]) => void;
+    change: (...args: any[]) => void;
+    compositionend: (...args: any[]) => void;
+    compositionstart: (...args: any[]) => void;
+    keydown: (...args: any[]) => void;
+    handleBlur: (...args: any[]) => void;
+    handleFocus: (...args: any[]) => void;
+}, string, import('vue').PublicProps, Readonly<EditorInputProps> & Readonly<{
+    "onUpdate:modelValue"?: ((...args: any[]) => any) | undefined;
+    onChange?: ((...args: any[]) => any) | undefined;
+    onCompositionend?: ((...args: any[]) => any) | undefined;
+    onCompositionstart?: ((...args: any[]) => any) | undefined;
+    onKeydown?: ((...args: any[]) => any) | undefined;
+    onHandleBlur?: ((...args: any[]) => any) | undefined;
+    onHandleFocus?: ((...args: any[]) => any) | undefined;
+}>, {
+    disabled: boolean;
+    placeholder: string;
+    modelValue: string;
+    autosize: boolean | {
+        minRows?: number;
+        maxRows?: number;
+    };
+    rows: number;
+    readOnly: boolean;
+    submitType: "enter" | "shiftEnter";
+}, {}, {}, {}, string, import('vue').ComponentProvideOptions, false, {
+    editableElement: HTMLDivElement;
+}, HTMLDivElement>;
+export default _default;
